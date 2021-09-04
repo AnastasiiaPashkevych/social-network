f51aedcabac89fa5524b2ac6d24b2f2d6debc60f
Anastasia <nastyapashkevich10@gmail.com>
2021-09-04 17:01:44 +0200

    feat: create dispatch method

--- src/App.js
+++ src/App.js
@@ -21,8 +21,7 @@ function App(props) {
             <Route path='/profile'
                    render={() => <Profile
                        profilePage={props.state.profilePage}
-                       addPost={props.addPost}
-                       updateNewPostText={props.updateNewPostText}/>}/>
+                       dispatch={props.dispatch}/>}/>
             <Route path='/news'
                    render={() => <News/>}/>
             <Route path='/music'
