c87faa1217518b947f9a639a215356ffa8b66749
Anastasia <nastyapashkevich10@gmail.com>
2021-09-04 16:06:43 +0200

    feat: create store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import state, {addPost, subscribe, updateNewPostText} from "./redux/state";
+import store from "./redux/state";
 import ReactDOM from "react-dom";
 import App from "./App";
 
@@ -9,16 +9,16 @@ let rerenderEntireTree = (state) => {
   ReactDOM.render(
       <React.StrictMode>
         <App state={state}
-             addPost={addPost}
-             updateNewPostText={updateNewPostText}/>
+             addPost={store.addPost.bind(store)}
+             updateNewPostText={store.updateNewPostText.bind(store)}/>
       </React.StrictMode>,
       document.getElementById('root')
   );
 }
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,56 +1,57 @@
-let rerenderEntireTree = () => {
-  console.log('State changed')
-}
-
-let state = {
-  profilePage: {
-    posts: [
-      {id: 1, message: 'Hi, how are you?', likesCount: 12},
-      {id: 2, message: 'It is my first post', likesCount: 1},
-      {id: 3, message: 'Fine', likesCount: 212},
-      {id: 4, message: 'Yo', likesCount: 0},
-      {id: 5, message: 'Yo', likesCount: 67},
-    ],
-    newPostText: 'Write something here',
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        {id: 1, message: 'Hi, how are you?', likesCount: 12},
+        {id: 2, message: 'It is my first post', likesCount: 1},
+        {id: 3, message: 'Fine', likesCount: 212},
+        {id: 4, message: 'Yo', likesCount: 0},
+        {id: 5, message: 'Yo', likesCount: 67},
+      ],
+      newPostText: 'Write something here',
+    },
+    dialogsPage: {
+      dialogs: [
+        {id: 1, name: 'Valentina'},
+        {id: 2, name: 'Dimych'},
+        {id: 3, name: 'Olena'},
+        {id: 4, name: 'Andrew'},
+        {id: 5, name: 'Sveta'},
+      ],
+      messages: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'Fine'},
+        {id: 4, message: 'Yo'},
+        {id: 5, message: 'Yo'},
+      ],
+    },
+    sidebar: {},
   },
-  dialogsPage: {
-    dialogs: [
-      {id: 1, name: 'Valentina'},
-      {id: 2, name: 'Dimych'},
-      {id: 3, name: 'Olena'},
-      {id: 4, name: 'Andrew'},
-      {id: 5, name: 'Sveta'},
-    ],
-    messages: [
-      {id: 1, message: 'Hi'},
-      {id: 2, message: 'How are you?'},
-      {id: 3, message: 'Fine'},
-      {id: 4, message: 'Yo'},
-      {id: 5, message: 'Yo'},
-    ],
+  getState() {
+    return this._state;
   },
-  sidebar: {},
+  _callSubscriber() {
+    console.log('State changed')
+  },
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0,
+    };
+
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = '';
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
+  },
 }
 
-export const addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 0,
-  };
-
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = '';
-  rerenderEntireTree(state);
-}
-
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-}
-
-export default state;
+export default store;
