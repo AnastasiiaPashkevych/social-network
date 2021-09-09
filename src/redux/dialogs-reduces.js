3cef3aa60582214dff96708f86a0f5d9bd98fca2
Anastasia <nastyapashkevich10@gmail.com>
2021-09-09 16:53:45 +0200

    feat: make the copy of the state in ProfileReducer

--- src/redux/dialogs-reduces.js
+++ src/redux/dialogs-reduces.js
@@ -21,15 +21,17 @@ let initialState = {
 
 const dialogsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case UPDATE_NEW_MESSAGE_BODY:
+    case UPDATE_NEW_MESSAGE_BODY: {
       state.newMessageBody = action.body;
       return state;
+    }
 
-    case SEND_MESSAGE:
+    case SEND_MESSAGE: {
       let body = state.newMessageBody;
       state.messages.push({id: 6, message: body},);
       state.newMessageBody = '';
       return state;
+    }
 
     default:
       return state;
