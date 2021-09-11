2076fad7f6ec89ebe7586b332291ec92fbd76636
Anastasia <nastyapashkevich10@gmail.com>
2021-09-11 13:30:24 +0200

    feat: use function setUsers

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,6 +1,85 @@
 import styles from './Users.module.css';
 
 function Users(props) {
+
+  if (props.users.length === 0) {
+    props.setUsers([
+          {
+            id: 1,
+            photoUrl: 'https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png',
+            followed: true,
+            fullName: 'Dmitry',
+            status: 'I am a boss',
+            location: {city: 'Minsk', country: 'Belarus'}
+          },
+          {
+            id: 2,
+            photoUrl: 'https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png',
+            followed: true,
+            fullName: 'Alex',
+            status: 'I am a singer',
+            location: {city: 'Moskow', country: 'Russia'}
+          },
+          {
+            id: 3,
+            photoUrl: 'https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png',
+            followed: false,
+            fullName: 'Alena',
+            status: 'I am a boss',
+            location: {city: 'Minsk', country: 'Belarus'}
+          },
+          {
+            id: 4,
+            photoUrl: 'https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png',
+            followed: true,
+            fullName: 'Katya',
+            status: 'I am a boss',
+            location: {city: 'Kiev', country: 'Ukraine'}
+          },
+          {
+            id: 5,
+            photoUrl: 'https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png',
+            followed: true,
+            fullName: 'Anna',
+            status: 'I am a boss',
+            location: {city: 'Minsk', country: 'Belarus'}
+          },
+          {
+            id: 6,
+            photoUrl: 'https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png',
+            followed: false,
+            fullName: 'Artsiom',
+            status: 'I am a boss',
+            location: {city: 'Minsk', country: 'Belarus'}
+          },
+          {
+            id: 7,
+            photoUrl: 'https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png',
+            followed: true,
+            fullName: 'Oleg',
+            status: 'I am a boss',
+            location: {city: 'Minsk', country: 'Belarus'}
+          },
+          {
+            id: 8,
+            photoUrl: 'https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png',
+            followed: false,
+            fullName: 'Elena',
+            status: 'I am a boss',
+            location: {city: 'Minsk', country: 'Belarus'}
+          },
+          {
+            id: 9,
+            photoUrl: 'https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png',
+            followed: true,
+            fullName: 'Olga',
+            status: 'I am a boss',
+            location: {city: 'Minsk', country: 'Belarus'}
+          },
+        ],
+    )
+  }
+
   return (
       <div>
         {
