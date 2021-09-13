a5ef9fa0bdf3ab11a01caf66e0bd3ead7ebcd1fa
Anastasia <nastyapashkevich10@gmail.com>
2021-09-13 15:51:05 +0200

    feat: use users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,83 +1,14 @@
 import styles from './Users.module.css';
+import * as axios from "axios";
+import userPhoto from './../../assets/images/user.png'
 
 function Users(props) {
 
   if (props.users.length === 0) {
-    props.setUsers([
-          {
-            id: 1,
-            photoUrl: 'https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png',
-            followed: true,
-            fullName: 'Dmitry',
-            status: 'I am a boss',
-            location: {city: 'Minsk', country: 'Belarus'}
-          },
-          {
-            id: 2,
-            photoUrl: 'https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png',
-            followed: true,
-            fullName: 'Alex',
-            status: 'I am a singer',
-            location: {city: 'Moskow', country: 'Russia'}
-          },
-          {
-            id: 3,
-            photoUrl: 'https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png',
-            followed: false,
-            fullName: 'Alena',
-            status: 'I am a boss',
-            location: {city: 'Minsk', country: 'Belarus'}
-          },
-          {
-            id: 4,
-            photoUrl: 'https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png',
-            followed: true,
-            fullName: 'Katya',
-            status: 'I am a boss',
-            location: {city: 'Kiev', country: 'Ukraine'}
-          },
-          {
-            id: 5,
-            photoUrl: 'https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png',
-            followed: true,
-            fullName: 'Anna',
-            status: 'I am a boss',
-            location: {city: 'Minsk', country: 'Belarus'}
-          },
-          {
-            id: 6,
-            photoUrl: 'https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png',
-            followed: false,
-            fullName: 'Artsiom',
-            status: 'I am a boss',
-            location: {city: 'Minsk', country: 'Belarus'}
-          },
-          {
-            id: 7,
-            photoUrl: 'https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png',
-            followed: true,
-            fullName: 'Oleg',
-            status: 'I am a boss',
-            location: {city: 'Minsk', country: 'Belarus'}
-          },
-          {
-            id: 8,
-            photoUrl: 'https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png',
-            followed: false,
-            fullName: 'Elena',
-            status: 'I am a boss',
-            location: {city: 'Minsk', country: 'Belarus'}
-          },
-          {
-            id: 9,
-            photoUrl: 'https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png',
-            followed: true,
-            fullName: 'Olga',
-            status: 'I am a boss',
-            location: {city: 'Minsk', country: 'Belarus'}
-          },
-        ],
-    )
+
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+      props.setUsers(response.data.items)
+    });
   }
 
   return (
@@ -86,7 +17,7 @@ function Users(props) {
           props.users.map(u => <div className={styles.user} key={u.id}>
             <div className={styles.photoButton}>
               <div>
-                <img src={u.photoUrl} alt="" className={styles.userPhoto}/>
+                <img src={u.photos.small !== null ? u.photos.small : userPhoto} alt="" className={styles.userPhoto}/>
               </div>
               <div>
                 {u.followed
@@ -101,12 +32,12 @@ function Users(props) {
 
             <div className={styles.userInfo}>
               <div>
-                <div>{u.fullName}</div>
+                <div>{u.name}</div>
                 <div className={styles.userStatus}>{u.status}</div>
               </div>
               <div>
-                <div>{u.location.country}</div>
-                <div className={styles.userCity}>{u.location.city}</div>
+                <div>{"u.location.country"}</div>
+                <div className={styles.userCity}>{"u.location.city"}</div>
               </div>
             </div>
           </div>)
